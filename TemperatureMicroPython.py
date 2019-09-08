ae81bd619ad52d55b6edec77873c6f52e7bf4ce2
Luca <tankado@live.it>
2019-09-08 12:34:22 +0200

    Update TemperatureMicroPython.py

--- TemperatureMicroPython.py
+++ TemperatureMicroPython.py
@@ -1,4 +1,3 @@
-
 import dht
 import machine
 import time
@@ -6,17 +5,17 @@ from umqtt.simple import MQTTClient
 
 
 def temperature():
-  while(True):
-      d = dht.DHT22(machine.Pin(4))
-      d.measure()
-      print(str(d.temperature()) + " " + str(d.humidity()))
-      c = MQTTClient("client", "192.168.4.4")
-      try:
-        c.connect()
-        c.publish("temperature", str(d.temperature()) + " " + str(d.humidity()))
-      except:
-        time.sleep(1)
-      time.sleep(5)
-      
+    while(True):
+        try:
+            d = dht.DHT22(machine.Pin(4))
+            d.measure()
+        
+            c = MQTTClient('client', '192.168.4.4')
+        
+            c.connect()
+            c.publish('temperature', str(d.temperature()) + ' ' + str(d.humidity()))
+        except:
+            time.sleep(1)
+        time.sleep(5)
 
 temperature()
